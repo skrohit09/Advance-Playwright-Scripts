524c05ce0be986b66e895960479b377c37d5af83
skrohit09 <rohithbk09@gmail.com>
2026-03-07 12:09:26 +0530

    Added scenario to test multi user log with each use having diffrent user experience

--- tests/multiUserLogIn.spec.js
+++ tests/multiUserLogIn.spec.js
@@ -1,22 +1,49 @@
 const { test, expect } = require('@playwright/test');
+const usersData = require('../Utils/Swag_labs users/users.json');
 
-
-test("Swag labs", async ({ page }) =>{
-    await page.goto("https://www.saucedemo.com/");
-    //await page.getByRole('text', {name: "Username"}).fill("standard_user");
-    //await page.getByTestId('username').fill("standard_user");
-    await page.locator("#user-name").fill("standard_user");
-    await page.getByRole('password',{name: "password"}).fill("secret_sauce");
-    await page.getByRole('submit', {name : "login-button"}).click();
-    await page.getByRole('button', {name: "add-to-cart-sauce-labs-backpack"}).click();
-    await page.getByTestId('shopping-cart-link').click();
-    await page.getByTestId('checkout').click();
-    await page.getByRole('text', {name: "firstName"}).fill("Sk");
-    await page.getByRole('text', {name: "last-name"}).fill("Rohit");
-    await page.getByRole('text', {name: "postalCode"}).fill("1234");
-    await page.getByTestId('continue').click();
-    await page.getByTestId('finish').click();
-    await expect(page.getByTestId('complete-header')).toHaveText("Thank you for your order!");
-    await page.getByTestId('back-to-products').click();
-    await expect(page.getByRole('button', {name: "add-to-cart-sauce-labs-backpack"})).toBeVisible();
-})
+test.describe('Multi-User Login Tests', () => {
+    usersData.users.forEach((user) => {
+        test(`Login with ${user.username}`, async ({ page }) => {
+            await page.goto('https://www.saucedemo.com/');
+            await page.locator('[data-test="username"]').fill(user.username);
+            await page.locator('[data-test="password"]').fill(user.password);
+            const startTime = Date.now();
+            await page.locator('[data-test="login-button"]').click();
+            const endTime = Date.now();
+            if (endTime - startTime > 3000) {
+                console.log("this page is slow 😒, there is a performence glitch");
+            }
+            if(user.username === 'locked_out_user'){
+                await expect(page.getByText('Epic sadface: Sorry, this user has been locked out.')).toBeVisible();
+                return;
+            }
+            
+            await expect(page.locator(".app_logo")).toBeVisible();
+            
+            await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+            await page.locator('[data-test="shopping-cart-link"]').click();
+            await page.locator('[data-test="checkout"]').click();
+            await page.locator('[data-test="firstName"]').fill('Sk');
+            await page.locator('[data-test="lastName"]').fill('Rohit');
+           const lastNameValue = await page.locator('[data-test="lastName"]').inputValue();
+            if (lastNameValue === '') {
+                console.log("This user have a problem 😒");
+                return;
+            }
+            await page.locator('[data-test="postalCode"]').click();
+            await page.locator('[data-test="postalCode"]').fill('1234');
+            await page.locator('[data-test="continue"]').click();
+            await page.locator('[data-test="finish"]').click();
+            await expect(page.locator('[data-test="back-to-products"]')).toBeVisible();
+            const startTime1 = Date.now();
+            await page.locator('[data-test="back-to-products"]').click();
+            const endTime1 = Date.now();
+            if (endTime - startTime > 3000) {
+                console.log("this page is slow 😒, there is a performence glitch");
+            }
+            await page.getByRole('button', { name: 'Open Menu' }).click();
+            await page.locator('[data-test="logout-sidebar-link"]').click();
+            await expect(page.locator('[data-test="login-button"]')).toBeVisible();
+        });
+    });
+});
